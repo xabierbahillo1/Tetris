0b41b86f758f68fe34e195b431f5c4e6bd616b89
Xabier <xabierbahillo1@gmail.com>
2021-03-20 18:55:47 +0100

    Empezado ejercicio 2

--- tetris.js
+++ tetris.js
@@ -58,7 +58,9 @@ function Block (pos, color) {
 	// para establecer la anchura del bloque y la anchura de la línea.
 
 	//Calculo posicion inicial
-	this.init(new Point(pos.x*Block.BLOCK_SIZE,pos.y*Block.BLOCK_SIZE),new Point((pos.x*Block.BLOCK_SIZE)+Block.BLOCK_SIZE,(pos.y*Block.BLOCK_SIZE)+Block.BLOCK_SIZE)); //Paso de cuadrado a pixeles
+	var cuadradoSize=Block.BLOCK_SIZE - Block.OUTLINE_WIDTH*2;
+
+	this.init(new Point(pos.x*cuadradoSize,pos.y*cuadradoSize),new Point((pos.x*cuadradoSize)+cuadradoSize,(pos.y*cuadradoSize)+Block.cuadradoSize)); //Paso de cuadrado a pixeles
 	this.color=color;
 	this.lineWidth=Block.OUTLINE_WIDTH;
 
@@ -72,6 +74,146 @@ Block.BLOCK_SIZE = 30;
 Block.OUTLINE_WIDTH = 2;
 
 // TU CÓDIGO: emplea el patrón de herencia (Block es un Rectangle)
+
+// ************************************
+// *      EJERCICIO 2                  *
+// ************************************
+
+// ============== Shape ===================================
+
+function Shape() {}
+
+
+Shape.prototype.init = function(coords, color) {
+
+	// TU CÓDIGO AQUÍ: método de inicialización de una Pieza del tablero
+	// Toma como parámetros: coords, un array de posiciones de los bloques
+	// que forman la Pieza y color, un string que indica el color de los bloques
+	// Post-condición: para cada coordenada, crea un bloque de ese color y lo guarda en un bloque-array.
+	this.blockList=[];
+	for (var i=0; i<coords.length;i++){
+		this.blockList[i]=new Block(coords[i],color);
+	}
+};
+
+Shape.prototype.draw = function() {
+
+	// TU CÓDIGO AQUÍ: método que debe pintar en pantalla todos los bloques
+	// que forman la Pieza
+	for (var i=0;i<this.blockList.length;i++){
+		this.blockList[i].draw();
+	}
+};
+
+// ============= I_Shape ================================
+function I_Shape(center) {
+	var coords = [new Point(center.x - 2, center.y),
+		new Point(center.x - 1, center.y),
+		new Point(center.x , center.y),
+		new Point(center.x + 1, center.y)];
+
+	Shape.prototype.init.call(this, coords, "blue");
+}
+// TU CÓDIGO AQUÍ: La clase I_Shape hereda de la clase Shape
+I_Shape.prototype = new Shape();
+I_Shape.prototype.constructor = Shape;
+
+// =============== J_Shape =============================
+function J_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: Para programar J_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x - 1, center.y),
+		new Point(center.x , center.y),
+		new Point(center.x +1, center.y+1),
+		new Point(center.x + 1, center.y)];
+
+	Shape.prototype.init.call(this, coords, "orange");
+
+}
+
+// TU CÓDIGO AQUÍ: La clase J_Shape hereda de la clase Shape
+J_Shape.prototype = new Shape();
+J_Shape.prototype.constructor = Shape;
+
+// ============ L Shape ===========================
+function L_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: Para programar L_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x - 1, center.y+1),
+		new Point(center.x - 1, center.y),
+		new Point(center.x , center.y),
+		new Point(center.x + 1, center.y)
+	];
+
+	Shape.prototype.init.call(this, coords, "cyan");
+}
+
+// TU CÓDIGO AQUÍ: La clase L_Shape hereda de la clase Shape
+L_Shape.prototype = new Shape();
+L_Shape.prototype.constructor = Shape;
+
+// ============ O Shape ===========================
+function O_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: Para programar O_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x - 1, center.y+1),
+		new Point(center.x - 1, center.y),
+		new Point(center.x, center.y+1),
+		new Point(center.x , center.y)];
+
+	Shape.prototype.init.call(this, coords, "red");
+}
+
+// TU CÓDIGO AQUÍ: La clase O_Shape hereda de la clase Shape
+O_Shape.prototype = new Shape();
+O_Shape.prototype.constructor = Shape;
+
+// ============ S Shape ===========================
+function S_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: Para programar S_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x - 1, center.y+1),
+		new Point(center.x, center.y+1),
+		new Point(center.x+1, center.y),
+		new Point(center.x , center.y)];
+
+	Shape.prototype.init.call(this, coords, "green");
+}
+
+// TU CÓDIGO AQUÍ: La clase S_Shape hereda de la clase Shape
+S_Shape.prototype = new Shape();
+S_Shape.prototype.constructor = Shape;
+
+// ============ T Shape ===========================
+function T_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: : Para programar T_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x-1, center.y),
+		new Point(center.x, center.y+1),
+		new Point(center.x , center.y),
+		new Point(center.x+1, center.y)];
+
+	Shape.prototype.init.call(this, coords, "yellow");
+}
+
+// TU CÓDIGO AQUÍ: La clase T_Shape hereda de la clase Shape
+T_Shape.prototype = new Shape();
+T_Shape.prototype.constructor = Shape;
+
+// ============ Z Shape ===========================
+function Z_Shape(center) {
+
+	// TU CÓDIGO AQUÍ: : Para programar Z_Shape toma como ejemplo el código de la clase I_Shape
+	var coords = [new Point(center.x+1, center.y+1),
+		new Point(center.x , center.y),
+		new Point(center.x-1, center.y),
+		new Point(center.x, center.y+1)];
+
+	Shape.prototype.init.call(this, coords, "magenta");
+
+}
+Z_Shape.prototype = new Shape();
+Z_Shape.prototype.constructor = Shape;
 
 
 
